57f8a34733a4488842b6443f069409fa8f68660a
jhbui1 <jhbui1@uci.edu>
2020-04-30 22:08:47 -0500

    plans, login/vets forms

--- fur2feathers/src/app/vets/vets.component.ts
+++ fur2feathers/src/app/vets/vets.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-vets',
   templateUrl: './vets.component.html',
-  styleUrls: ['./vets.component.css']
+  styleUrls: ['./vets.component.scss']
 })
 export class VetsComponent implements OnInit {
-
+  sel_species:string = "";
+  vet_zip:string = ""; 
+  vet_state:string ="";
+  vet_city:string = ""; 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(`${this.sel_species} ${this.vet_zip} ${this.vet_state} ${this.vet_state}`);
   }
 
 }

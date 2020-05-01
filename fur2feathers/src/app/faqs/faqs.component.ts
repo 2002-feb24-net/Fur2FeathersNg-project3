ea7299451c7077dd54768807e1e98b67c32c266e
jhbui1 <jhbui1@uci.edu>
2020-05-01 10:41:03 -0500

    web.config

--- fur2feathers/src/app/faqs/faqs.component.ts
+++ fur2feathers/src/app/faqs/faqs.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface faq { 
+  question:string,
+  answer:string
+}
+
 @Component({
   selector: 'app-faqs',
   templateUrl: './faqs.component.html',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqsComponent implements OnInit {
 
+  faqs:faq[] = [];
+
   constructor() { }
 
+  initializeFaqs() {
+    for(let i=0;i<5;i++){
+      // this.faqs.push(faq)
+    }
+  }
+
+  showOrHide() {
+    
+  }
   ngOnInit(): void {
+    this.initializeFaqs();
   }
 
 }

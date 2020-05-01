ed328c431dfe3658ed5bc2055cd7b5a11f81fa6a
jhbui1 <jhbui1@uci.edu>
2020-04-30 19:34:28 -0500

    login

--- fur2feathers/src/app/log-in/log-in.component.ts
+++ fur2feathers/src/app/log-in/log-in.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+  styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
-
+  email: string ="";
+  pwd: string = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit() {
+    console.log("login")
+    console.log(this.email+this.pwd);
   }
 
 }

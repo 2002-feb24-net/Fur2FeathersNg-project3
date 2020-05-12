36aa0257830646947f7c7082e30c173bfcb9286b
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-05-12 00:09:06 -0500

    switch to new setup for https with https domains

--- fur2feathers/src/environments/environment.prod.ts
+++ fur2feathers/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  f2fApiBaseUrl: 'http://a21d80b4b9e0e455cac78b6313b1027d-1369705032.us-east-2.elb.amazonaws.com'
+  // f2fApiBaseUrl: 'http://a21d80b4b9e0e455cac78b6313b1027d-1369705032.us-east-2.elb.amazonaws.com'
   // f2fApiBaseUrl: 'http://localhost:8000'
+  f2fApiBaseUrl: 'https://f2f-api.2002.revaturelabs.com'
 };
